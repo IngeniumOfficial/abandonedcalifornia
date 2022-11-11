55e955b3b8dc216ae35e0549fa0584294afbf0bc
yermie <yermolovpersonal@gmail.com>
2022-11-11 15:36:29 -0800

    Basic nodejs server

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,10 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 
 // https://astro.build/config
+import node from "@astrojs/node";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
+  output: "static"
 });
