37e822115005071c62c2a3c109c82f06c0dc27b8
yermie <yermolovpersonal@gmail.com>
2022-11-11 14:13:53 -0800

    Added basic mapping functionality

--- src/components/CaliMap.jsx
+++ src/components/CaliMap.jsx
@@ -1,32 +1,38 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import * as d3 from 'd3';
+import * as L from 'leaflet';
+import { MapContainer, useMap, TileLayer, Marker, Popup } from 'react-leaflet';
+import abandonedcalifornia from '../assets/abandonedcalifornia.json';
 
 import '../styles/components/CaliMap.scss';
 
+const CalStateBounds = [
+    [ 42.234305, -125.267535 ],
+    [ 32.592792, -113.720101 ]
+]
+
+
 export default function CaliMap() {
-    const svgRef = useRef();
-
-    var w = 760;
-    var h = 600;
-
-    //Define map projection
-	var projection = d3.geoMercator()
-        .center([ -120, 37 ])
-        .translate([ w/2, h/2 ])
-        .scale([ w*3.3 ]);
-
-    //Define path generator
-    var path = d3.geoPath()
-        .projection(projection);
-
-    const selectedSVG = d3.select(svgRef);
-
+    const mapRef = useRef();
+    
 
 
     return (
-        <div>
+        <div id='CaliMapMain'>
             <h2>This is the CaliMap component</h2>
-            <svg id='mainsvg' ref={svgRef}></svg>
+            <MapContainer id='mapContainer' maxBounds={CalStateBounds} center={[37.42076433334728, -120.57716717888397]} zoom={7} scrollWheelZoom={true}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                {abandonedcalifornia.map((town) => 
+                    <Marker
+                        key={town.id}
+                        position={[ town.gps.latitude, town.gps.longitude]}
+                    >
+                    </Marker>
+                )}
+            </MapContainer>
         </div>
     )
 }
