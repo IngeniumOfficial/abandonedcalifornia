4e17ca1b7b102726ccfc3020ef9845eba3c53046
yermie <yermolovpersonal@gmail.com>
2022-11-11 15:22:15 -0800

    Basic frontend render

--- src/components/CaliMap.jsx
+++ src/components/CaliMap.jsx
@@ -4,12 +4,26 @@ import * as L from 'leaflet';
 import { MapContainer, useMap, TileLayer, Marker, Popup } from 'react-leaflet';
 import abandonedcalifornia from '../assets/abandonedcalifornia.json';
 
+import mappinfilledblack from '../../src/assets/mappinfilledblack.svg';
+import mappinfilledred from '../../src/assets/mappinfilledred.svg';
+
 import '../styles/components/CaliMap.scss';
 
 const CalStateBounds = [
     [ 42.234305, -125.267535 ],
     [ 32.592792, -113.720101 ]
 ]
+
+const blackIcon = L.icon({
+    iconUrl: mappinfilledblack,
+    shadowSize: [68, 95]
+})
+
+const redIcon = L.icon({
+    iconUrl: mappinfilledred,
+    shadowSize: [68, 95]
+})
+
 
 
 export default function CaliMap() {
@@ -25,13 +39,27 @@ export default function CaliMap() {
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                {abandonedcalifornia.map((town) => 
-                    <Marker
-                        key={town.id}
-                        position={[ town.gps.latitude, town.gps.longitude]}
-                    >
-                    </Marker>
-                )}
+                {abandonedcalifornia.map((town) => {
+                    if(town.gps.presence === 'accurate'){
+                        return(
+                            <Marker
+                                icon={blackIcon}
+                                key={town.id}
+                                position={[ town.gps.latitude, town.gps.longitude]}
+                            >
+                            </Marker>
+                        )
+                    } else {
+                        return(
+                            <Marker
+                                icon={redIcon}
+                                key={town.id}
+                                position={[ town.gps.latitude, town.gps.longitude]}
+                            >
+                            </Marker>
+                        )
+                    }
+                })}
             </MapContainer>
         </div>
     )
